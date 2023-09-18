2cdc54e546107fef639e02c15cd585c57a997aba
Anton Sievering <anton@sievering.de>
2023-09-18 15:25:32 +0200

    improved formatting tool

--- format_text.py
+++ format_text.py
@@ -8,12 +8,25 @@ input_file = open(sys.argv[1], "r")
 lines = input_file.readlines()
 input_file.close()
 
+def split_word(word):
+    front = ""
+    
+    for s in word:
+        if s.isalpha():
+            front += s
+        else:
+            return front, word[len(front):]
+    
+    return word, ""
+
 output_file = open(sys.argv[2], "w")
 for line in lines:
     line = line.replace("\n", "")
     words = line.split(" ")
-    string = ""
+    marked_words = []
     for word in words:
         if word != "":
-            string += f"<tmark>{word}</tmark>"
-    output_file.write(string + "<br/>\n")
+            front, back = split_word(word);
+            marked_words.append(f"<tmark>{front}</tmark>{back}")
+    output_file.write(" ".join(marked_words) + "<br/>\n")
+output_file.close()
